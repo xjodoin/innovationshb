a55d9466713a55bbe92deb2adafe4af3f72c31e7
Xavier Jodoin <xjodoin@gmail.com>
2011-11-22 21:49:47 -0500

    add description travaux

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,26 +26,30 @@ public class Application extends Controller {
 	}
 
 	public static void quotationRequest(String name, String email,
-			String phone, String address) throws EmailException {
-		SimpleEmail confirEmail = new SimpleEmail();
-		confirEmail.setFrom("info@innovationshb.com");
-		confirEmail.addTo(email);
-		confirEmail.setSubject("Confirmation demande  de soumission");
-		confirEmail
-				.setMsg("Votre demande a bien été reçu. \nNotre équipe vous contactera dans les plus bref délais. \n\nL'équpe InnovationsHB \ninfo@innovationshb.com");
-		Mail.send(confirEmail);
+			String phone, String address,String work) throws EmailException {
+		try {
+			SimpleEmail confirEmail = new SimpleEmail();
+			confirEmail.setFrom("info@innovationshb.com");
+			confirEmail.addTo(email);
+			confirEmail.setSubject("Confirmation demande  de soumission");
+			confirEmail
+					.setMsg("Votre demande a bien été reçu. \nNotre équipe vous contactera dans les plus bref délais. \n\nL'équpe InnovationsHB \ninfo@innovationshb.com");
+			Mail.send(confirEmail);
 
-		SimpleEmail simpleEmail = new SimpleEmail();
-		simpleEmail.setFrom("info@innovationshb.com");
-		simpleEmail.addTo("m.bellemare@innovationshb.com");
-		simpleEmail.addTo("f.bellemare@innovationshb.com");
-		
-		simpleEmail.setSubject("Demande  de soumission");
-		simpleEmail
-				.setMsg("Une demande de soumission a été fait par : \nNom: "
-						+ name + "\nCourriel: " + email + "\nTéléphone: "
-						+ phone + "\nAdresse :" + address);
-		Mail.send(simpleEmail);
+			SimpleEmail simpleEmail = new SimpleEmail();
+			simpleEmail.setFrom("info@innovationshb.com");
+			simpleEmail.addTo("m.bellemare@innovationshb.com");
+			simpleEmail.addTo("f.bellemare@innovationshb.com");
+			
+			simpleEmail.setSubject("Demande  de soumission");
+			simpleEmail
+					.setMsg("Une demande de soumission a été fait par : \nNom: "
+							+ name + "\nCourriel: " + email + "\nTéléphone: "
+							+ phone + "\nAdresse :" + address+"\nTravaux à effectuer: "+work);
+			Mail.send(simpleEmail);
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
 
 		redirect("/");
 	}
